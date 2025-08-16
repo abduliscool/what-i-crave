7a34cdc2e986a837a6f1ddb43808dd2c82d1469f
mmuqsit3 <mmuqsit3@gatech.edu>
2025-08-16 18:31:04 -0400

    Connected to MongoDB cluster

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const connectDB = require('./db'); // Import the connection function
+
+// Connect to the database
+connectDB();
+
 const app = express();
 const PORT = 5000;
 
-// A simple test route to make sure the server is working
 app.get('/api', (req, res) => {
   res.json({ message: "Hello from the WhatICrave backend!" });
 });
 
-// This line starts the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
